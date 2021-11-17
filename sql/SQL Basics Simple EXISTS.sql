6bd5511b777eab4e5b2a0b243cbeb137f5f62edd
phukyawwin <phukyawwin96@gmail.com>
2021-11-17 19:47:15 +0630

    SQL Basics: Simple EXISTS

--- sql/SQL Basics Simple EXISTS.sql
+++ sql/SQL Basics Simple EXISTS.sql
@@ -1,3 +1,4 @@
+-- to create a SELECT statement that will contain data about departments that had a sale with a price over 98.00 dollars
 select  id,name from departments where exists (
 	select distinct department_id from sales where
   	departments.id=sales.department_id and price>98.00) order by id;
